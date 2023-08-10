ec9786ae9dedec0542e5248404c5db9e9164ea71
Erfan AmanAbadi <erfan80amani23@gmail.com>
2023-08-10 17:08:12 +0330

    styled header and footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 export default function Footer() {
   return (
-    <div className="footer">
-      <h1
-        style={{
-          fontSize: "15px",
-        }}
-      >
-        آدرس شرکت: تهران – خیابان آزادی – بلوار اکبری – کوچه شهید عباس شرقی –
-        نبش خیابان صادقی – پلاک ۲
-      </h1>
+    <div className="mx-2 my-2 py-6 bg-blue-950 text-white rounded-xl">
+      <div className="flex flex-col items-center gap-4">
+        <h1 className="text-2xl font-extrabold">حسابا</h1>
+
+        <h3 className="text-center">
+          آدرس شرکت: تهران – خیابان آزادی – بلوار اکبری – کوچه شهید عباس شرقی –
+          نبش خیابان صادقی – پلاک ۲
+        </h3>
+      </div>
     </div>
   );
 }
--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,15 +1,42 @@
 import React from "react";
+import ShoppingCartIcon from "@heroicons/react/24/outline/ShoppingCartIcon";
+import MagnifyingGlassIcon from "@heroicons/react/24/outline/MagnifyingGlassIcon";
+
 export default function Header() {
   return (
-    <div className="header">
-      <h1
-        style={{
-          color: "white",
-          fontSize: "19px",
-        }}
-      >
-        تسک فنی موقعیت شغلی فرانت دولوپر شرکت حسابا
-      </h1>
+    <div className="flex items-center justify-between w-full py-4 px-4 shadow-md">
+      <div>
+        <h1 className="text-lg font-bold">حسابا</h1>
+      </div>
+
+      <div className="">
+        <div className="flex items-center gap-4 w-72 2lg:w-[652px] py-2 px-3 bg-gray-100 rounded-md">
+          <MagnifyingGlassIcon className="w-6 h-6 text-gray-400" />
+
+          <input
+            className="bg-transparent flex-1 placeholder:text-xs"
+            placeholder="محصول مورد نظرتان را جستجو کنید"
+          />
+        </div>
+      </div>
+
+      <div className="flex gap-4">
+        <div className="flex items-center border border-gray-600 rounded-lg">
+          <button className="px-5 py-2">ورود</button>
+          <div className="h-[70%] w-[1px] bg-gray-600" />
+          <button className="px-5 py-2">ثبت نام</button>
+        </div>
+
+        <button className="relative border px-2 rounded-md">
+          <ShoppingCartIcon className="w-5 h-5" />
+
+          {false && (
+            <span className="absolute bottom-[1px] right-[1px] flex items-center justify-center w-4 h-4 rounded-full bg-red-500 text-white text-xs">
+              1
+            </span>
+          )}
+        </button>
+      </div>
     </div>
   );
 }
