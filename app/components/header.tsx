1396920010a9baf73321038724a33fcf5146ffbd
Erfan AmanAbadi <erfan80amani23@gmail.com>
2023-08-11 10:35:00 +0330

    show cart count on header

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -2,8 +2,13 @@ import React from "react";
 import ShoppingCartIcon from "@heroicons/react/24/outline/ShoppingCartIcon";
 import MagnifyingGlassIcon from "@heroicons/react/24/outline/MagnifyingGlassIcon";
 import SearchInput from "./Search";
+import { useSelector } from "react-redux";
+import { RootState } from "../redux/store";
 
 export default function Header() {
+  const cartItems = useSelector((state: RootState) => state.cart.items);
+  const cartCount = cartItems.reduce((prev, cur) => prev + cur.count, 0);
+
   return (
     <div className="flex items-center justify-between w-full py-4 px-4 shadow-md">
       <div className="flex items-center gap-8">
@@ -26,9 +31,9 @@ export default function Header() {
         <button className="relative border px-2 rounded-md">
           <ShoppingCartIcon className="w-5 h-5" />
 
-          {false && (
+          {cartCount && (
             <span className="absolute bottom-[1px] right-[1px] flex items-center justify-center w-4 h-4 rounded-full bg-red-500 text-white text-xs">
-              1
+              {cartCount}
             </span>
           )}
         </button>
