9afb78d49c18722d2fd6e49bd0eacc090b0fdf49
Erfan AmanAbadi <erfan80amani23@gmail.com>
2023-08-11 10:35:19 +0330

    fixed single product link route

--- app/routes/shop/components/SingleProduct.tsx
+++ app/routes/shop/components/SingleProduct.tsx
@@ -4,7 +4,7 @@ import { Product } from "../../../types/Product";
 
 const SingleProduct = ({ data }: { data: Product }) => {
   return (
-    <Link to={`/product/${data.id}`}>
+    <Link to={`/products/${data.id}`}>
       <div className="p-4 border shadow rounded-md flex flex-col gap-4 text-left">
         <div className="w-[90%] mx-auto ">
           <img src={data.image} className="object-contain rounded-sm" />
